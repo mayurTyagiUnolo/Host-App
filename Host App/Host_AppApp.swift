b244e9c3f4fa9ed716c921bdfb22bb4e13c6f745
Mayur <mayurtyagi@unolo.com>
2024-07-18 18:24:12 +0530

    Initial Commit

---
+++ Host App/Host_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Host_AppApp.swift
+//  Host App
+//
+//  Created by Mayur  on 18/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Host_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
